8545782a84e03c394f841e779d6db8af117a6cc5
Dhanush Naik <77430067+dhanushlnaik@users.noreply.github.com>
2021-08-07 11:24:56 +0530

    Custom Help Command

--- cogs/help.py
+++ cogs/help.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 from random import *
 from db import check_prefix
-Thugesh = 725612078764785694 # Main Server
+Thugesh = 725612078764785694
 
 # __COLORS__
 color1 = discord.Color.from_rgb(93,71,82)
@@ -20,44 +20,60 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx, cog:str=None):
-        lst = []
         if cog is None:
             helpEmbedall = discord.Embed(color=ctx.author.color, description=f"These Are the Available Commands For {self.bot.user.name}\nBot's Global Prefix Is - `amy `\nServer Prefix Is - `{check_prefix(ctx.guild.id)}`")
             helpEmbedall.set_author(name="Amy Sensei - Help", icon_url=self.bot.user.avatar_url)
             helpEmbedall.set_thumbnail(url=ctx.author.avatar_url)
-    
-            helpEmbedall.add_field(name="__Fun__ [18]", value=f"`cat` `dog` `imgur` `gif` `marry` `div` `sib` `poke` `tellme` `rps` `random` `truth` `dare` `drunkify` `gay` `ship` `disown` `urban`", inline=False)
-            helpEmbedall.add_field(name="__Misc__ [8]", value=f"`botstats` `ping` `prefix` `say` `pre` `owo` `avatar` `whois`", inline=False)
+            if ctx.guild.id == Thugesh:
+                helpEmbedall.add_field(name="__Study__ [3]", value=f"`stats` `leaderboard` `doubtadd`", inline=False)
+            helpEmbedall.add_field(name="__Fun__ [24]", value=f"`cat` `dog` `imgur` `gif` `marry` `div` `sib` `poke` `tellme` `rps` `random` `truth` `dare` `drunkify` `gay` `ship` `disown` `urban` `family` `joke` `insult` `quote` `nhie` `wyr`", inline=False)
+            helpEmbedall.add_field(name="__Actions__ [9]", value=f"`pat` `hug` `kiss` `bite` `slap` `handhold` `tickle` `bully` `kill` `nom` `cuddle` `wave` `greet` `punch`", inline=False)
+            helpEmbedall.add_field(name="__Misc__ [8]", value=f"`botstats` `ping` `prefix` `say` `pre` `owo` `avatar` `whois` `upvote`", inline=False)
             helpEmbedall.add_field(name="__ToDo__ [3]", value=f"`setup_todo` `remove_todo` `todo`", inline=False)
             helpEmbedall.add_field(name="__Support__ [3]",value=f"`server` `github` `invite`" )
             await ctx.channel.send(embed=helpEmbedall)
-
-        elif cog.lower() == '3' or cog.lower()=="todo":
+            return
+        if cog.lower() == 'study' and ctx.guild.id == Thugesh:
+            helpEmbedcog = discord.Embed(color=ctx.author.color, description='*Commands directly related to Study functionality. [Thugesh Only]*')
+            helpEmbedcog.set_author(name="Amy Sensei - Help", icon_url=self.bot.user.avatar_url)
+            helpEmbedcog.set_thumbnail(url=ctx.author.avatar_url)
+            helpEmbedcog.add_field(name="Commands :", value=f"`[1] stats <user>` - Shows Users Study Stats. \n`[2] leaderboard [d | w | m | total | doubts] ` - Checkout Server's Study Leaderboard.\n`[3] doubtadd <user> [points in integer]` - Add Doubt Points [Admin Only] ", inline=False)
+            await ctx.channel.send(embed=helpEmbedcog)
+            return
+        elif cog.lower() in ["4", "todo", "setup_todo", "remove_todo"]:
             helpEmbedcog = discord.Embed(color=ctx.author.color, description='*Commands directly related to Todo functionality.*')
             helpEmbedcog.set_author(name="Amy Sensei - Help", icon_url=self.bot.user.avatar_url)
             helpEmbedcog.set_thumbnail(url=ctx.author.avatar_url)
-            helpEmbedcog.set_footer(text=f"Page 3 of 4")
+            helpEmbedcog.set_footer(text=f"Page 4 of 5")
             helpEmbedcog.add_field(name="Commands :", value=f"`[1] setup_todo <#channel>` - Set Server's Todo Channel \n`[2] remove_todo` - Disables Todo Functionality\n`[3] todo task1;task2;task3` - Make your interactive Todo List!", inline=False)
             await ctx.channel.send(embed=helpEmbedcog)
             return
+        elif cog.lower() in ["2", "pat", "action", "hug","kiss","slap","handhold","tickle","bully","kill","bite","nom", "cuddle", "wave", "greet", "punch"]:
+            helpEmbedcog = discord.Embed(color=ctx.author.color, description='*Commands directly related to Todo functionality.*')
+            helpEmbedcog.set_author(name="Amy Sensei - Help", icon_url=self.bot.user.avatar_url)
+            helpEmbedcog.set_thumbnail(url=ctx.author.avatar_url)
+            helpEmbedcog.set_footer(text=f"Page 2 of 5")
+            helpEmbedcog.add_field(name="Commands :", value=f"`[1] pat <user>` - Pat Someone \n`[2] hug <user>` - Hug Someone \n`[3] kiss <user>` - Kiss Someone \n`[4] slap <user>` - Slap Someone \n`[5] handhold <user>` - Handhold Someone \n`[6] tickle <user>` - Tickle Someone \n`[7] bully <user>` - Bully Someone \n`[8] kill <user>` - Kill Someone \n`[9] bite <user>` - Bite Someone \n`[10] nom <user>` - Nom on Someone \n`[11] cuddle <user>` - Cuddle Someone \n`[12] wave <user>` - Wave Someone \n`[13] greet <user>` - Greet Someone \n`[14] punch <user>` - Punch Someone \n", inline=False)
+            await ctx.channel.send(embed=helpEmbedcog)
+            return
         
-        elif cog.lower() == '1' or cog.lower()=="fun":
+        elif cog.lower() in ["1", "fun", "cat","dog","imgur","gif","marry","div","sib","poke","tellme","rps","random","truth","dare","drunkify","gay","ship","disown","urban","family"]:
             helpEmbedcog = discord.Embed(color=ctx.author.color, description='*Commands directly related to Fun functionality.*')
             helpEmbedcog.set_author(name="Amy Sensei - Help", icon_url=self.bot.user.avatar_url)
             helpEmbedcog.set_thumbnail(url=ctx.author.avatar_url)
-            helpEmbedcog.set_footer(text=f"Page 1 of 4")
-            helpEmbedcog.add_field(name="Commands :", value=f"`[1] cat` - Random Cats! \n`[2] dog` - Random Dogs!\n`[3] imgur <search>` - Search for an image from imgur.\n`[4] gif <search>` - Search for an GIF. \n`[5] marry <member>` - Marry Someone\n`[6] div` - Divorce\n`[7] sib <member>` - Make Someone your E-sibling!\n`[8] poke <member>` - Poke Someone!\n`[9] tellme <statement>` - I'll tell if your statement is True or not!!\n`[10] rps` - Rock , Paper , Scissor!\n`[11] random <r/reddit>` - Random Reddit Post!\n`[12] truth` - Truth Question\n`[13] dare` - Dare Question!\n`[14] drunkify <text>` - Drunkify Some Random Text\n`[15] gay <user>` - UMM ! Very Mature Command XD\n`[16] ship <user1><user2>` - Ahem\n`[17] disown <user>` - Disown someone who is your Sibling.\n`[18] urban <word>` - Find Defination of a word.", inline=False)
+            helpEmbedcog.set_footer(text=f"Page 1 of 5")
+            helpEmbedcog.add_field(name="Commands :", value=f"`[1] cat` - Random Cats! \n`[2] dog` - Random Dogs!\n`[3] imgur <search>` - Search for an image from imgur.\n`[4] gif <search>` - Search for an GIF. \n`[5] marry <member>` - Marry Someone\n`[6] div` - Divorce\n`[7] sib <member>` - Make Someone your E-sibling!\n`[8] poke <member>` - Poke Someone!\n`[9] tellme <statement>` - I'll tell if your statement is True or not!!\n`[10] rps` - Rock , Paper , Scissor!\n`[11] random <r/reddit>` - Random Reddit Post!\n`[12] truth` - Truth Question\n`[13] dare` - Dare Question!\n`[14] drunkify <text>` - Drunkify Some Random Text\n`[15] gay <user>` - UMM ! Very Mature Command XD\n`[16] ship <user1><user2>` - Ahem\n`[17] disown <user>` - Disown someone who is your Sibling.\n`[18] urban <word>` - Find Defination of a word.\n`[19] family <user>` - Find Family Tree of a User.\n`[20] joke` - Random Joke\n`[21] quote` - Random Quote.\n`[22] nhie` - Never have I Ever.\n`[23] wyr` - Would You Rather.\n`[24] insult @user` - Insults A User.", inline=False)
             await ctx.channel.send(embed=helpEmbedcog)
             return
-        elif cog.lower() in ['misc', 'miscellaneous', 'other','2']:
+        elif cog.lower() in ['misc', 'miscellaneous', 'other','3']:
             helpEmbedcog = discord.Embed(color=ctx.author.color, description='*Commands directly related to Miscellaneous functionality.*')
             helpEmbedcog.set_author(name="Amy Sensei - Help", icon_url=self.bot.user.avatar_url)
             helpEmbedcog.set_thumbnail(url=ctx.author.avatar_url)
-            helpEmbedcog.set_footer(text=f"Page 2 of 4")
-            helpEmbedcog.add_field(name="Commands :", value=f"`[1] botstats`  - Show Bot Stats!\n`[2] ping` - Checkout whether bot is online.\n`[3] prefix` - Returns prefix of the bot.\n`[4] say [msg]` - Make bot say something!\n`[5] pre <newprefix>` - Change Prefix of the bot.\n`[6] owo [msg]` - Any Message to owo\n`[7] avatar <user>` - Shows Avatar of a User.\n`[8] whois <user>` - Shows User Info", inline=False)
+            helpEmbedcog.set_footer(text=f"Page 3 of 5")
+            helpEmbedcog.add_field(name="Commands :", value=f"`[1] botstats`  - Show Bot Stats!\n`[2] ping` - Checkout whether bot is online.\n`[3] prefix` - Returns prefix of the bot.\n`[4] say [msg]` - Make bot say something!\n`[5] pre <newprefix>` - Change Prefix of the bot.\n`[6] owo [msg]` - Any Message to owo\n`[7] avatar <user>` - Shows Avatar of a User.\n`[8] whois <user>` - Shows User Info\n`[9] upvote`  - Returns Upvote Link!", inline=False)
             await ctx.channel.send(embed=helpEmbedcog)
             return
-        elif cog.lower() == "support"or cog.lower() == "4":
+        elif cog.lower() == "support"or cog.lower() == "5":
             helpEmbedcog = discord.Embed(color=ctx.author.color, description=f'**Developer - <@{624174437821972480}>**')
             helpEmbedcog.set_author(name="Amy Sensei - Help", icon_url=self.bot.user.avatar_url)
             helpEmbedcog.set_thumbnail(url=ctx.author.avatar_url)
